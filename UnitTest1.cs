97935d3e69203864e5c7013283ffa0d0c4ec9485
Gustavo Ferreira de Oliveira Coelho <gustavo.feneita1@gmail.com>
2024-01-15 17:07:50 -0300

    AulaMs

---
+++ UnitTest1.cs
@@ -0,0 +1,45 @@
+
+namespace AtividadeTeste;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void Teste_Calculo_IMC()
+    {
+        double imc_previsto = 31.25;
+        IMC i = new IMC();
+        i.peso = 80;
+        i.altura = 1.60;
+        i.Calcular_IMC();
+        Assert.AreEqual(imc_previsto, i.imc);
+    }
+
+    [DataTestMethod]
+    [DataRow("Abaixo do peso")]
+    [DataRow("Peso normal")]
+    [DataRow("Sobrepeso")]
+    [DataRow("Obesidade Grau I")]
+    [DataRow("Obesidade Grau II")]
+    [DataRow("Obesidade Grau III")]
+    public void Teste_Categoria_IMC(String cat)
+    {
+        IMC i = new IMC();
+        i.peso = 80;
+        i.altura = 1.60;
+        i.Calcular_IMC();
+        i.Classificar_IMC();
+        //MStest Assert.AreEqual
+        Assert.AreEqual(i.categoria, cat);
+    }
+
+    [TestMethod]
+    public void Soma()
+    {
+        int num1=10;
+        int num2=10;
+        var soma=num1+num2;
+         int somaEsperada=20;
+         Assert.AreEqual(soma, somaEsperada);
+    }
+}
